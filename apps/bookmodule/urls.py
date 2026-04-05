55969ca227a79cdaf096c7f1d7e0d574170a5435
LANA <your_email@gmail.com>
2026-04-05 15:16:45 +0300

    Complete Lab 6 HTML form and Django form handling

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -11,10 +11,12 @@ urlpatterns = [
     path('html5/listing', views.html5_listing, name="books.html5_listing"),
     path('html5/tables', views.html5_tables, name="books.html5_tables"),
 
-    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('search', views.search_books, name="books.search"),
 
     path('index2/<int:val1>/', views.index2),
     path('index2/<str:val1>/', views.index2_text),
     path('lab3/<int:bookId>/', views.viewbook_lab3),
     path('lab3_index/', views.index_lab3),
+
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
 ]
